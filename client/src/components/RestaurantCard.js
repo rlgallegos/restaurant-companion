9fee17239307c49dc960ca4eb7448dd27f3f3cba
Robert <rlgallegos85@gmail.com>
2023-05-08 19:49:49 -0400

    Updated Datbase and some tailwind

--- client/src/components/RestaurantCard.js
+++ client/src/components/RestaurantCard.js
@@ -7,12 +7,15 @@ function RestaurantCard({restaurant}) {
     function handleClick() {
         navigate(`user/${restaurant.id}/menu-display`)
     }
+    let splitURL = restaurant.url.split('.')
+    let formattedURL = 'www.' + [splitURL[1], splitURL[2]].join('.')
+
 
 
     return (
-        <div onClick={handleClick} className="bg-gray-100 bg-opacity-80 rounded-md shadow-md w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/4 p-12 mx-10 my-10 flex flex-col border border-transparent hover:border-gray-300 hover:transform hover:scale-110 transition-all duration-300 card">
-            <h3 className="text-lg font-bold flex-grow text-gray-700 my-auto">{restaurant.name}</h3>
-            <p className="my-1 ml-4  text-m flex-grow text-gray-600">{restaurant.url}</p>
+        <div onClick={handleClick} className="bg-gray-100 bg-opacity-80 rounded-md shadow-md  w-5/6 md:w-1/4 lg:w-1/3 xl:w-1/4 py-12 px-6 mx-10 my-10 items-center border border-transparent hover:border-gray-300 hover:transform hover:scale-110 transition-all duration-300 card">
+            <h3 className="text-lg font-bold text-gray-700 my-auto">{restaurant.name}</h3>
+            <p className="text-m text-gray-600 mx-0 break-words">{formattedURL}</p>
         </div>
     )
 }
