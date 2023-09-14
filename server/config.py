0d459467d095f10768658daaff8af33817364d3d
Robert <rlgallegos85@gmail.com>
2023-09-14 18:24:56 -0400

    sqlalchemy session interface

--- server/config.py
+++ server/config.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
-from flask_session import Session
+from flask_session import Session, SqlAlchemySessionInterface
 
 from data_sets import ingredient_names
 
@@ -16,6 +16,8 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
+db = SQLAlchemy(metadata=metadata)
+
 
 app = Flask(
     __name__,
@@ -24,6 +26,7 @@ app = Flask(
     template_folder='../client/build'
 )
 
+migrate = Migrate(app, db)
 
 bcrypt = Bcrypt(app)
 
@@ -35,20 +38,29 @@ print('app secret key in config.py', app.secret_key)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['SESSION_TYPE'] = 'sqlalchemy'  # Use SQLAlchemy as the session interface
-app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-app.config['SESSION_COOKIE_SECURE'] = True
-app.config['SESSION_USE_SIGNER'] = True
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_COOKIE_NAME'] = 'manage_cookie'
+
+# app.config['SESSION_TYPE'] = 'sqlalchemy'  # Use SQLAlchemy as the session interface
+# app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+# app.config['SESSION_COOKIE_SECURE'] = True
+# app.config['SESSION_USE_SIGNER'] = True
+# app.config['SESSION_PERMANENT'] = False
+# app.config['SESSION_COOKIE_NAME'] = 'manage_cookie'
 
 app.config['CORS_HEADERS'] = 'Content-Type'
-Session(app)
+session_interface = SqlAlchemySessionInterface(
+    app=app,
+    db=db,
+    table='your_session_table',  # Replace with your desired session table name
+    key_prefix='your_prefix',  # Replace with your desired session key prefix
+    use_signer=True,  # Whether to sign the session id cookie or not
+    permanent=False  # Whether to use permanent sessions or not
+)
+
+
+
+Session(app, session_interface=session_interface)
 CORS(app, supports_credentials=True, origin='https://restaurant-companion.vercel.app')
 
 app.json.compact = False
 
-
-db = SQLAlchemy(metadata=metadata)
-migrate = Migrate(app, db)
 db.init_app(app)
