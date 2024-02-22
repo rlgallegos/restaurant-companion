ff571915f5a6ff92634ff90c11f363e43ead5bbd
Robert <rlgallegos85@gmail.com>
2024-02-21 22:03:44 -0500

    setting up lower nav bar

---
+++ client/app/(components)/LowerNavBar.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+const LowerNavBar: React.FC = () => {
+    return (
+        <nav className="flex h-full w-full">
+            <div className="h-full w-1/3 bg-blue-200 items-center justify-center">
+                <h1 className="text-4xl">The Restaurant Companion</h1>
+            </div>
+            <div className="h-full w-1/3 bg-blue-400"></div>
+            <div className="h-full w-1/3 bg-blue-600"></div>
+        </nav>
+    )
+}
+export default LowerNavBar
--- client/app/(routes)/(user)/restaurants/page.tsx
+++ client/app/(routes)/(user)/restaurants/page.tsx
@@ -2,25 +2,29 @@
 import { getAllRestaurants } from "@/app/services/user"
 import { RestaurantBasicType } from "@/app/types"
 
+import LowerNavBar from "@/app/(components)/LowerNavBar"
+
 const Restaurants: React.FC = async () => {
     const restaurants: RestaurantBasicType[] = await getAllRestaurants()
 
     const restaurantCards = restaurants.map(restaurant => {
         return (
             // Individual Card
-            <div key={restaurant.id} className="h-48 w-96 bg-slate-200 flex items-center justify-center">
+            <div key={restaurant.id} className="h-48 w-96 bg-slate-200 flex items-center justify-center cursor-pointer border border-transparent rounded-3xl hover:border-secondary">
                 <h2>{restaurant.name}</h2>
             </div>
         )
     })
     return (
         <main className="min-h-screen flex flex-col items-center justify-center bg-white-2 bg-center bg-cover">
-            <nav className="h-48 w-full flex items-center justify-between px-24">
+            <nav className="h-48 w-full flex items-center px-24">
                 <h1 className="text-4xl">Restaurants</h1>
-                <div>Sort</div>
             </nav>
             <div className="h-screen w-screen flex flex-wrap gap-12 items-cente justify-evenly overflow-y-scroll">
                 {restaurantCards}
+            </div>
+            <div className="h-[33vh] w-full">
+                <LowerNavBar />
             </div>
         </main>
     )
